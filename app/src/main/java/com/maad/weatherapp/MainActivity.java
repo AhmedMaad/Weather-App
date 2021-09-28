837542c9766de0cdf084cd101ba54ec964b37a7d
AhmedMaad <atef.ahmed1341@gmail.com>
2021-09-28 14:41:15 +0200

    Adding Dependencies

--- app/src/main/java/com/maad/weatherapp/MainActivity.java
+++ app/src/main/java/com/maad/weatherapp/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // https://api.openweathermap.org/data/2.5/onecall?lat=26.8206&lon=30.8025&exclude=minutely,hourly&units=metric&appid=e0d008a472d038e5c02bc7df0ab25c14
+    // Icons: http://openweathermap.org/img/wn/{01d}@2x.png
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
